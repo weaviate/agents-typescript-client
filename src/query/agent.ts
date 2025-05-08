37bd094b407b59b3cc42244a2e5b583e307690fa
Augustas <augustas.skaburskas@gmail.com>
2025-05-08 16:00:35 +0300

    add jscods

--- src/query/agent.ts
+++ src/query/agent.ts
@@ -2,10 +2,26 @@ import { WeaviateClient } from "weaviate-client";
 import { QueryAgentResponse } from "./response/response.js";
 import { mapResponse } from "./response/response-mapping.js";
 
+/**
+ * An agent for executing agentic queries against Weaviate.
+ *
+ * Warning:
+ * Weaviate Agents - Query Agent is an early stage alpha product.
+ * The API is subject to breaking changes. Please ensure you are using the latest version of the client.
+ *
+ * For more information, see the [Weaviate Query Agent Docs](https://weaviate.io/developers/agents/query)
+ */
 export class QueryAgent {
   private systemPrompt?: string;
   private agentsHost: string;
 
+  /**
+   * Creates a new QueryAgent instance.
+   *
+   * @param client - The Weaviate client instance.
+   * @param collections - The collections to query.
+   * @param options - Additional options for the QueryAgent.
+   */
   constructor(
     private client: WeaviateClient,
     private collections: string[],
@@ -18,6 +34,13 @@ export class QueryAgent {
     this.agentsHost = agentsHost;
   }
 
+  /**
+   * Run the query agent.
+   *
+   * @param query - The natural language query string for the agent.
+   * @param options - Additional options for the run.
+   * @returns The response from the query agent.
+   */
   async run(
     query: string,
     { viewProperties }: QueryAgentRunOptions = {}
@@ -51,11 +74,16 @@ export class QueryAgent {
   }
 }
 
+/** Options for the QueryAgent. */
 export type QueryAgentOptions = {
+  /** System prompt to guide the agent's behavior. */
   systemPrompt?: string;
+  /** Host of the agents service. */
   agentsHost?: string;
 };
 
+/** Options for the QueryAgent run. */
 export type QueryAgentRunOptions = {
+  /** List of of property names the agent has the ability to view across all collections. */
   viewProperties?: string[];
 };
